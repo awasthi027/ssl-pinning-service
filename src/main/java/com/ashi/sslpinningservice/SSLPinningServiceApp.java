fe4ea73f6307230cd3d35dd772e18f1d972c0d28
A <A>
2026-06-18 11:47:22 +0530

    Updated new services

--- src/main/java/com/ashi/sslpinningservice/SSLPinningServiceApp.java
+++ src/main/java/com/ashi/sslpinningservice/SSLPinningServiceApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SSLPinningServiceApplication {
+public class SSLPinningServiceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SSLPinningServiceApplication.class, args);
+        SpringApplication.run(SSLPinningServiceApp.class, args);
     }
 }
 
