fd330e190aa9bea5aabb9e32c682b8d378f220d2
A <A>
2026-06-18 11:40:38 +0530

    Renamed application file name

--- src/main/java/com/ashi/sslpinningservice/SslPinningServiceApplication.java
+++ src/main/java/com/ashi/sslpinningservice/SslPinningServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SslPinningServiceApplication {
+public class SSLPinningServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SslPinningServiceApplication.class, args);
+        SpringApplication.run(SSLPinningServiceApplication.class, args);
     }
 }
 
